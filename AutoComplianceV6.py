f5dbb30f598c26e2e9cf5aeba4be086a7f17ddeb
nicolasaparicio <31460300+nicolasaparicio@users.noreply.github.com>
2024-03-13 12:24:00 -0400

    Update AutoComplianceV6.py
    adding in blacklist functionality, header freeze, and auto sort

--- AutoComplianceV6.py
+++ AutoComplianceV6.py
@@ -5,6 +5,7 @@ from os import system
 import pandas as pd
 import argparse
 import re
+from datetime import datetime
 from alive_progress import alive_bar
 from alive_progress.styles import showtime, Show
 
@@ -18,17 +19,19 @@ from alive_progress.styles import showtime, Show
 
 ####################################
 
+cdt = datetime.now() # Get the local date and time
+
+op = '%s_%s_%s' % (cdt.month, cdt.day, cdt.year)
 
 
 
 
 
 #Global Variables
-spreadsheetName = "Auto_Comp_March02_24.xlsx"
+spreadsheetName = "Auto_Comp_black_list_" + op + ".xlsx"
 
 spreadsheetloc = str(os.getcwd()) + "/" + spreadsheetName
 workbook = xlsxwriter.Workbook(spreadsheetloc)
-
 
 ### Create raw report template ###
 
@@ -62,7 +65,13 @@ format_red = workbook.add_format({'bg_color': 'red',
                                     'align': 'top',
                                     'text_wrap': True })
 #used for header row and comments column
-format_gray = workbook.add_format({'bg_color': 'gray',
+format_gray = workbook.add_format({'bg_color': 'd8d8d8',
+                                    'font_name': 'Calibri',
+                                    'font_size': 11,
+                                    'bold': True,
+                                    'align': 'Center',
+                                    'text_wrap': True })
+format_grayer = workbook.add_format({'bg_color': 'E8e8e8',
                                     'font_name': 'Calibri',
                                     'font_size': 11,
                                     'bold': True,
@@ -91,19 +100,21 @@ normalBOLDtext = workbook.add_format({
                                     'text_wrap': True })
 
 columnHeaderFormatting = [
-    ['A:A', 11, 'A1','IP(s)'],
+    ['A:A', 10, 'A1','IP(s)'],
     ['B:B', 15, 'B1', 'Source/PluginID'],
-    ['C:C', 9, 'C1', 'Risk Level'],
+    ['C:C', 12, 'C1', 'Risk Level'],
     ['D:D', 45, 'D1', 'Finding Name'],
     ['E:E', 45, 'E1', 'Finding Details'],
     ['F:F', 45, 'F1', 'Recommendation'],
     ['G:G', 48, 'G1', 'Current Settings'],
     ['H:H', 5, 'H1', ''],
-    ['I:I', 18, 'I1', 'Policy Requirements'], # remove this 
-    ['J:J', 12, 'J1', 'STIG-ID'],
+    ['I:I', 25, 'I1', 'Policy Requirements'],
+    ['J:J', 10, 'J1', 'STIG-ID'],
     ['K:K', 20, 'K1', 'STIG-Name' ],
-    ['L:L', 45, 'L1', 'Nessus (Full Output)'],
-    ['M:M', 19, 'M1', 'Comments']
+    ['L:L', 16, 'L1', 'DHS Requirements'],
+    ['M:M', 45, 'M1', 'Nessus (Full Output)'],
+    ['N:N', 19, 'N1', 'SSS Comments'],
+    ['O:O', 19, 'O1', 'INL Comments']
 ]
 # TODO:
 #This version has a bug when database schema for column nessus_host_id is not unique, it will add those IP's as findings incorrectly
@@ -114,33 +125,51 @@ def combine_csvs(dir_path):
         os.remove('output.csv')
     if os.path.isfile('output.txt'):
         os.remove('output.txt')
+    if os.path.isfile('blacklist.csv'):
+        os.remove('blacklist.csv')
     # TODO: Maybe set "SC detected" here and new list if SC is detected, which would change cms...
     df = pd.DataFrame()
     file_csv_path = []
     file_txt_path = []
+    black_List = []
+    bl_flag = 0
     for i, file_name in enumerate(os.listdir(dir_path)):
         if file_name.endswith('csv'):
             file_csv_path.append(os.path.join(dir_path, file_name))
+    try: 
+        #have your blacklist as a CSV in a directory "BlackList"
+        for i, file_name in enumerate(os.listdir(dir_path+'/BlackList')):
+            if file_name.endswith('csv'):
+                black_List.append(os.path.join(str(dir_path+'/BlackList/'), file_name))
+        bl_flag = 1
+    except:
+        print("No blackList")
+
     for i, file_name in enumerate(os.listdir(dir_path)):
         if file_name.endswith('txt'):
             file_txt_path.append(os.path.join(dir_path, file_name))
-    
+
     if file_txt_path:
         index = 0
         worksheets = workbook_format(len(file_csv_path))
         print("Compliance File Path")
-
         for item in file_csv_path:
             df = pd.read_csv(item)
             df.to_csv('output.csv', index=True)
             df_txt = pd.concat(map(pd.read_csv, file_txt_path))
             df_txt.to_csv('output.txt', index=False)
-            
-            get_compliance('output.csv',"output.txt", dir_path, index, worksheets)
+            if(bl_flag):
+                df_blacklist = pd.concat(map(pd.read_csv, black_List))
+                df_blacklist.to_csv('blacklist.csv', index=True)
+            else:
+                df_blacklist = []
+            get_compliance('output.csv',"output.txt",df_blacklist, dir_path, index, worksheets)
             if os.path.isfile('output.csv'):
                 os.remove('output.csv')
             if os.path.isfile('output.txt'):
                 os.remove('output.txt')
+            if os.path.isfile('blacklist.csv'):
+                os.remove('blacklist.csv')
             index += 1
 
         workbook.close()
@@ -153,7 +182,7 @@ def combine_csvs(dir_path):
             print("-----Reading in: ", item)
             df = pd.read_csv(item)
             df.to_csv('output.csv', mode='a', index=False, header=True)
-            get_compliance_config_path('output.csv', dir_path, index, worksheets)
+            get_compliance_config_path('output.csv', dir_path, index, worksheets, item)
             if os.path.isfile('output.csv'):
                 os.remove('output.csv')
             index += 1
@@ -189,13 +218,13 @@ def compute(total, failedItems_df):
                 category_string = str(row['Cross References']).split("CAT #",1)[1].split(",")[0]
                 if(category_string == "I"):
                 # row.at[index,'Severity']='HIGH'
-                    row.replace(row['Severity'], "HIGH", inplace=True)
+                    row.replace(row['Severity'], "HIGH (CAT I)", inplace=True)
                 elif(category_string == "II"):
                     # row.at[,'Severity']='MEDIUM'
-                    row.replace(row['Severity'], "MEDIUM", inplace=True)
+                    row.replace(row['Severity'], "MEDIUM (CAT II)", inplace=True)
                     # row.loc[index,['Severity']] = 'MEDIUM'
                 else:
-                    row.replace(row['Severity'], "LOW", inplace=True)
+                    row.replace(row['Severity'], "LOW (CAT III)", inplace=True)
                 df_cat = df_cat._append(row, ignore_index=True)
             except:
                 row.replace(row['Severity'], "NA", inplace=True)
@@ -212,13 +241,13 @@ def compute_config_path(total, failedItems):
                 category_string = str(row['Cross References']).split("CAT|",1)[1].split(",")[0]
                 if(category_string == "I"):
                 # row.at[index,'Severity']='HIGH'
-                    row.replace(row['Risk'], "HIGH", inplace=True)
+                    row.replace(row['Risk'], "HIGH (CAT I)", inplace=True)
                 elif(category_string == "II"):
                     # row.at[,'Severity']='MEDIUM'
-                    row.replace(row['Risk'], "MEDIUM", inplace=True)
+                    row.replace(row['Risk'], "MEDIUM (CAT II)", inplace=True)
                     # row.loc[index,['Severity']] = 'MEDIUM'
                 else:
-                    row.replace(row['Risk'], "LOW", inplace=True)
+                    row.replace(row['Risk'], "LOW (CAT III)", inplace=True)
                 df_cat = df_cat._append(row, ignore_index=True)
             except:
                 row.replace(row['Risk'], "NA", inplace=True)
@@ -227,6 +256,20 @@ def compute_config_path(total, failedItems):
             bar()  
     return df_cat
 
+def vulnerbility_config_path(total, failedItems):
+    df_vul = pd.DataFrame()
+    with alive_bar(total) as bar:  # your expected total
+        for index, row in failedItems.iterrows():
+            try:
+                Vuln_ID_string = str(row['Description']).split("Vuln-ID|",1)[1].split("Policy")[0]
+                row.replace(row['Core Impact'], Vuln_ID_string, inplace=True)
+                df_vul = df_vul._append(row, ignore_index=True)
+
+            except:
+                df_vul = df_vul._append(row, ignore_index=True)
+
+            bar()  
+    return df_vul
 
 def workbook_format(file_list_length):
     # Create xlxs document:
@@ -261,18 +304,23 @@ def workbook_format(file_list_length):
 
     while j < file_list_length:
         for index in range(len(columnHeaderFormatting)):
-            worksheetList[j].set_column(columnHeaderFormatting[index][0], columnHeaderFormatting[index][1], format_gray)
+            if index == len(columnHeaderFormatting)-1:
+                worksheetList[j].set_column(columnHeaderFormatting[index][0], columnHeaderFormatting[index][1], format_grayer)
+            else:
+                worksheetList[j].set_column(columnHeaderFormatting[index][0], columnHeaderFormatting[index][1], format_gray)
             worksheetList[j].write(columnHeaderFormatting[index][2], columnHeaderFormatting[index][3], format_silver)
         j += 1
     return worksheetList
 
-def worksheet_writer(worksheets, file_index, row, testcount, plugin_output):
+def worksheet_writer(worksheets, df_blacklist, file_index, row, testcount, plugin_output):
     testcount = testcount + 1
-
+    blacklist_flag = False
     #seperate out data
     try:
         Vuln_ID_string = str(row['Cross References']).split("Vuln-ID #",1)[1].split(",")[0]
-
+        if df_blacklist['Vuln ID'].eq(Vuln_ID_string).any():
+            blacklist_flag = True
+            
     except:
         Vuln_ID_string = "NA"
             
@@ -345,7 +393,17 @@ def worksheet_writer(worksheets, file_index, row, testcount, plugin_output):
     worksheets[file_index].write_rich_string('K' + str(testcount), normalBOLDtext, 'STIG Policy Guideline: ', normaltext, STIG_Guideline,
         normalBOLDtext, '\nSTIG-USED: ', normaltext, STIG_ref_string, 
         normalBOLDtext)
-    worksheets[file_index].write('L' + str(testcount), str(row['Plugin']) + plugin_description + "\n"+ row["Cross References"], normaltext )
+    if blacklist_flag:
+        worksheets[file_index].write('L' + str(testcount), "Optional - DHS Blacklist", normaltext )
+    else:
+        worksheets[file_index].write('L' + str(testcount), "Required", normaltext )
+
+    worksheets[file_index].write('M' + str(testcount), str(row['Plugin']) + plugin_description + "\n"+ row["Cross References"], normaltext )
+    worksheets[file_index].autofilter('A1:O1')
+    worksheets[file_index].freeze_panes(1,0)
+    #autofit makes it look to long but is more readable uncomment to see after running
+    #worksheets[file_index].autofit()
+
     return testcount
 
 def worksheet_writer_config_path(worksheets, file_index, row, testcount):
@@ -358,7 +416,17 @@ def worksheet_writer_config_path(worksheets, file_index, row, testcount):
 
     except:
         Vuln_ID_string = "NA"
-
+        
+    try:
+        if df_blacklist['Vuln ID'].eq(Vuln_ID_string).any():
+            print("true: ", Vuln_ID_string)
+            blacklist_flag = "True"
+        else:
+            print("False: ", Vuln_ID_string)
+            blacklist_flag = "False"
+    except:
+        print("NA")
+        blacklist_flag = "NA"
     try:
         plugin_description = str(row['Description']).split("[FAILED]\n\n",1)[1].split("Solution:")[0] 
     except:
@@ -421,7 +489,15 @@ def worksheet_writer_config_path(worksheets, file_index, row, testcount):
     worksheets[file_index].write_rich_string('K' + str(testcount), normalBOLDtext, 'STIG Policy Guideline: ', normaltext, STIG_Guideline,
         normalBOLDtext, '\nSTIG-USED: ', normaltext, STIG_ref_string, 
         normalBOLDtext)
-    worksheets[file_index].write('L' + str(testcount), str(row['Description']), normaltext )
+    if blacklist_flag == "True":
+        worksheets[file_index].write('L' + str(testcount), "Optional - DHS Blacklist", normaltext )
+    elif blacklist_flag == "NA":
+        worksheets[file_index].write('L' + str(testcount), "NA", normaltext )
+    else:
+        worksheets[file_index].write('L' + str(testcount), "Required", normaltext )
+    worksheets[file_index].write('M' + str(testcount), str(row['Description']), normaltext )
+    worksheets[file_index].autofilter('A1:O1')
+    worksheets[file_index].freeze_panes(1,0)
     return testcount
 
 def get_failed_results(df, list_plugin_output):
@@ -430,17 +506,18 @@ def get_failed_results(df, list_plugin_output):
     print("Proccessing Failed Results")
     with alive_bar(len(df)) as bar: 
         for index, row in df.iterrows():
-
             #note the CSV contoining plugin output needs to be in this order [plugin, ip addres, severity] otherwise youll have an empty list
-            search_string =  str(str(row["Plugin"]) + "," + row["IP Address"] + "," + str(row["Severity"]) )
-            for item in list_plugin_output:
-                if search_string in item: 
-                    failed_items = failed_items._append(row, ignore_index=True)
-
+            try:
+                search_string =  str(str(row["Plugin"]) + "," + row["IP Address"] + "," + str(row["Severity"]) )
+                for item in list_plugin_output:
+                    if search_string in item: 
+                        failed_items = failed_items._append(row, ignore_index=True)
+            except:
+                print("is your .txt file in the right order? [Plugin, IP, Severity]")
             bar()
     return failed_items
-
-def get_compliance(file,file_txt, dir_path, file_index, worksheets):
+    
+def get_compliance(file,file_txt, df_blacklist, dir_path, file_index, worksheets):
     #Variables
     df_sorted = pd.DataFrame()
     ########## pandas stuff - actual sorting: #########
@@ -452,6 +529,8 @@ def get_compliance(file,file_txt, dir_path, file_index, worksheets):
     df_plugin_output = parse_plugin_out(df, plugin_output)
     #drop severity of type "info"
     df_noInfo = df[df.Severity != 'Info']
+
+
     #Grab failed results only
     failed_items = get_failed_results(df_noInfo, df_plugin_output)
     #join IP addresses
@@ -464,7 +543,6 @@ def get_compliance(file,file_txt, dir_path, file_index, worksheets):
     #Drop duplicate plugins
     df_combined_drop = failed_items.drop_duplicates(subset='Plugin')
 
-
     #Compute CAT
     failed_items=compute(len(df_combined_drop), df_combined_drop)
     # failed_items.to_csv('output2.csv', mode='a', index=False, header=True)
@@ -473,8 +551,10 @@ def get_compliance(file,file_txt, dir_path, file_index, worksheets):
     #Filter out NA (SC will not have full infomration for result error)
     df_noNA = failed_items[failed_items.Severity != 'NA']
 
+
     #Sort DF
-    custom_dict = {'HIGH': 0, 'MEDIUM': 1, 'LOW': 2}
+    custom_dict = {'HIGH (CAT I)': 0, 'MEDIUM (CAT II)': 1, 'LOW (CAT III)': 2}
+
     df_sorted = df_noNA.sort_values(by=['Severity'], key=lambda x: x.map(custom_dict))
 
     #used for debugging
@@ -490,10 +570,10 @@ def get_compliance(file,file_txt, dir_path, file_index, worksheets):
         for index, row in df_sorted.iterrows():
 
             #write to Excel doc
-            testcount = worksheet_writer(worksheets, file_index, row, testcount, plugin_output)
+            testcount = worksheet_writer(worksheets, df_blacklist, file_index, row, testcount, plugin_output)
             bar()
     
-def get_compliance_config_path(file, dir_path, file_index, worksheets):
+def get_compliance_config_path(file, dir_path, file_index, worksheets, item):
 
     ########## pandas stuff - actual sorting: #########
 
@@ -507,16 +587,22 @@ def get_compliance_config_path(file, dir_path, file_index, worksheets):
     except:
         print(f"Failed to parse file: {str(file)}")
 
-    failedItems = failedItems.drop_duplicates(subset='Description')
+    
 
 
     #update severity field based on value
     df_cat=compute_config_path(len(failedItems), failedItems)
+    if "Fire" not in item:
+        df_vul=vulnerbility_config_path(len(failedItems), failedItems)
+        df_vul = df_vul.drop_duplicates(subset='Core Impact')
 
-    #sort dataframe by severity 
-    custom_dict = {'HIGH': 0, 'MEDIUM': 1, 'LOW': 2, 'NA': 3}
-    df_sorted = df_cat.sort_values(by=['Risk'], key=lambda x: x.map(custom_dict))
-        
+        #sort dataframe by severity 
+        custom_dict = {'HIGH': 0, 'MEDIUM': 1, 'LOW': 2, 'NA': 3}
+        df_sorted = df_vul.sort_values(by=['Risk'], key=lambda x: x.map(custom_dict))
+    else:
+        df_vul = failedItems.drop_duplicates(subset="Description")
+        custom_dict = {'HIGH': 0, 'MEDIUM': 1, 'LOW': 2, 'NA': 3}
+        df_sorted = df_vul.sort_values(by=['Risk'], key=lambda x: x.map(custom_dict))        
 
     testcount = 1
     print("Writing to worksheets") 
